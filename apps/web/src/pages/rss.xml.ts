3f3a669e784c5a8f6cbe68b7b4b6164314e41fa1
hmziqrs <hmziqrs@gmail.com>
2026-03-07 20:38:18 +0500

    refactor(web): simplify RSS feed generation using @astrojs/rss
    Replace manual XML string construction with @astrojs/rss package for cleaner, more maintainable RSS feed generation. Adds language metadata to the RSS feed.

--- apps/web/src/pages/rss.xml.ts
+++ apps/web/src/pages/rss.xml.ts
@@ -1,3 +1,4 @@
+import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import type { APIContext } from "astro";
 
@@ -5,33 +6,16 @@ export async function GET(context: APIContext) {
   const posts = await getCollection("posts", ({ data }) => !data.draft);
   const sorted = posts.toSorted((a, b) => b.data.date.valueOf() - a.data.date.valueOf());
 
-  const site = context.site!.toString();
-
-  const items = sorted
-    .map(
-      (post) => `
-    <item>
-      <title><![CDATA[${post.data.title}]]></title>
-      <description><![CDATA[${post.data.description}]]></description>
-      <link>${site}posts/${post.id}/</link>
-      <guid>${site}posts/${post.id}/</guid>
-      <pubDate>${post.data.date.toUTCString()}</pubDate>
-    </item>`,
-    )
-    .join("");
-
-  const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
-  <channel>
-    <title>hmziq.rs</title>
-    <description>A blog about software engineering.</description>
-    <link>${site}</link>
-    <atom:link href="${site}rss.xml" rel="self" type="application/rss+xml" />
-    ${items}
-  </channel>
-</rss>`;
-
-  return new Response(xml, {
-    headers: { "Content-Type": "application/rss+xml; charset=utf-8" },
+  return rss({
+    title: "hmziq.rs",
+    description: "A blog about software engineering.",
+    site: context.site!,
+    items: sorted.map((post) => ({
+      title: post.data.title,
+      description: post.data.description,
+      pubDate: post.data.date,
+      link: `/posts/${post.id}/`,
+    })),
+    customData: `<language>en-us</language>`,
   });
 }
