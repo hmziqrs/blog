7baed71e704357cbee4913f44d18aed0d4d5159f
hmziqrs <hmziqrs@gmail.com>
2026-03-31 11:47:53 +0500

    chore: clean up code structure and remove unused code blocks

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,12 +1,13 @@
 // @ts-check
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
+import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
   output: "static",
   site: "https://hmziq.rs",
-  integrations: [sitemap()],
+  integrations: [sitemap(), icon()],
   vite: {
     plugins: [tailwindcss()],
   },
